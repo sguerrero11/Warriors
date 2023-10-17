a81f5b3d43c74ab22d4efd7b2068f15a250625a9
Santiago <s.guerrero@outlook.com>
2023-10-18 01:16:56 +0200

    Auto-saved

--- test-cases/java/Cucumber/Runner/runner.java
+++ test-cases/java/Cucumber/Runner/runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions (
-        features = {"test-cases/java/resources/features"},
+        features = {"test-cases/java/Cucumber/resources/features"},
         glue = {"Cucumber.StepsDefinition"}) // you need to show all the path to the package
 public class runner extends AbstractTestNGCucumberTests {
 /*
