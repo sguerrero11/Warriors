7dba405eb418cda3fd487cd9fab44827e5b92e54
Santiago <s.guerrero@outlook.com>
2023-10-27 03:48:28 +0200

    Auto-saved

--- src/jenkinsTests/jenkinsMessage.java
+++
@@ -1,8 +0,0 @@
-import org.testng.annotations.Test;
-
-public class jenkinsMessage {
-    @Test
-    public void echoMessage(){
-        System.out.println("This means that src is for test cases over Jenkins");
-    }
-}
